4d4bb997a7a86bfd743b0cedd0840f08a344c3f0
Cristopher Perez <cristopher240871@gmail.com>
2025-06-26 08:34:16 -0500

    [ADD][CP] trescloud_test: ADD INITIAL FILES

---
+++ trescloud_test/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- encoding:utf-8 -*-
+{
+    'name': 'Módulo para realizar modificaciones para la prueba de desarrolladores',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': '''
+        Este módulo contiene personalizaciones de acuerdo a las necesidades del cliente.
+
+        Autores:
+            -Cristopher Pérez
+    ''',
+    'author': 'TRESCLOUD',
+    'maintainer': 'TRESCLOUD',
+    'website': 'https://www.trescloud.com',
+    'summary': 'Módulo para realizar modificaciones para la prueba de desarrolladores',
+    'license': 'OEEL-1',
+    'depends': [
+
+    ],
+    'data': [
+        # data
+        # security
+        # reports
+        # wizard
+        # views
+    ],
+    "assets": {
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
